eb787444d0c650f8b43a4134b8e31dbde5a065f0
Yibo Duan <yibo.duan@gmail.com>
2014-11-15 17:38:36 -0500

    Fixed changed typo and directive

--- app/app.js
+++ app/app.js
@@ -5,14 +5,12 @@ app.directive('smthView', function() {
 		restrict: 'E',
 		templateUrl: 'views/smthView.html',
 		controller: function() {
-			app.controller('CreateSmthController', function() {
 			this.new_smth = {};
 
 			this.addSmth = function(smth) {
 				smth.info.push(this.new_smth);
 				this.new_smth = {};
 			};
-		});
 		},
 		controllerAs: 'smthCtrl'
 	};
