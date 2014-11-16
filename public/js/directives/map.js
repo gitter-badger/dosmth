df6945c2d9090e6916a1dc55d53f73d8e41334c8
David Jeong <davidyjeong@gmail.com>
2014-11-16 17:37:34 -0500

    changed to ng-map

---
+++ public/js/directives/map.js
@@ -0,0 +1,9 @@
+app.directive('smthMap', function(){  
+	return {
+	    restrict: 'E',
+	    templateUrl: 'views/mapView.html',
+		controller:function($scope){
+		},
+		controllerAs: 'panels'
+	};
+});
